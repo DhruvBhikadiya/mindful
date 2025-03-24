120b05776653efa7253fd6056c3a4cea53f1b70f
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2025-03-24 15:09:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,21 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 const siteconfigRoutes = require('./routes/siteconfigRoutes');
 const fileuploadRoutes = require("./routes/fileuploadRoutes");
 const contactleadRoutes = require("./routes/contactleadRoutes");
+const paymentsettingRoutes = require('./routes/paymentsettingRoutes');
 const aboutusRoutes = require('./routes/aboutusRoutes');
 const treatmentRoutes = require('./routes/treatmentRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const userRoutes = require('./routes/userRoutes');
+const appointmentRoutes = require('./routes/appointmentRoutes');
+const prescriptionRoutes = require('./routes/prescriptionRoutes');
+const policyRoutes = require('./routes/policyRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // CORS options
 const corsOptions = {
-  origin: '*', // If you want any URL then use '*'
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204
@@ -31,8 +37,14 @@ app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/siteconfig', siteconfigRoutes);
 app.use("/api/file", fileuploadRoutes);
 app.use('/api/contactlead', contactleadRoutes);
+app.use('/api/paymentsetting', paymentsettingRoutes);
 app.use('/api/aboutus', aboutusRoutes);
 app.use('/api/treatment', treatmentRoutes);
+app.use('/api/blog', blogRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/appointment', appointmentRoutes);
+app.use('/api/prescription', prescriptionRoutes);
+app.use('/api/policy', policyRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
